2e262785749b3711998ca504457ee4103e0c080a
Wahyu081912 <fatina0819@gmail.com>
2024-03-29 23:58:58 +0700

    change ke 10

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-createApp(App).mount('#App')
 
-import { createApp } from 'vue'
-import App from '/src/App.vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap'; 
 import '/src/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
+import { createApp } from 'vue'
+import App from '/src/App.vue'
 
-
+createApp(App).mount('#App')

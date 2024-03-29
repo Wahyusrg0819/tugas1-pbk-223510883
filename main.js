445828c0ac2e3f71257daff4c26b69012a85f1ae
Wahyu081912 <fatina0819@gmail.com>
2024-03-29 21:51:09 +0700

    mengubah tittle mengubah sesuatu

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap'; 
-import './style.css'
+import '/src/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '/src/App.vue'
 
 createApp(App).mount('#app')
